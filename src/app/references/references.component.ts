af0bb8d0f6087a8168874e8c3e494224f9ef2c1d
Eugen1188 <eugen.ferchow@gmx.de>
2026-02-25 01:36:02 +0100

    change svg, add mobile-slider, add font Inter

--- src/app/references/references.component.ts
+++ src/app/references/references.component.ts
@@ -9,14 +9,17 @@ import { Component } from '@angular/core';
 })
 export class ReferencesComponent {
   slides = [
-    ['img/door-ref.jpeg'],
+    ['img/windowref2_before.jpeg', 'img/windowref2_after.jpeg'],
+    ['img/windowref_before.jpeg', 'img/windowref_after.jpeg'],
     ['img/garage.jpeg'],
     ['img/slidingdoor.jpeg'],
-    ['img/windowref_before.jpeg', 'img/windowref_after.jpeg'],
-    ['img/windowref2_before.jpeg', 'img/windowref2_after.jpeg'],
+    ['img/door-ref.jpeg'],
   ];
 
   currentIndex = 0;
+  touchStartX = 0;
+  touchEndX = 0;
+  swipeThreshold = 50;
 
   next() {
     if (this.currentIndex < this.slides.length - 1) {
@@ -36,5 +39,24 @@ export class ReferencesComponent {
 
   goToSlide(index: number) {
     this.currentIndex = index;
+  }
+
+  onTouchStart(event: TouchEvent) {
+    this.touchStartX = event.changedTouches[0].clientX;
+  }
+
+  onTouchEnd(event: TouchEvent) {
+    this.touchEndX = event.changedTouches[0].clientX;
+    this.handleSwipe();
+  }
+
+  handleSwipe() {
+    const distance = this.touchEndX - this.touchStartX;
+
+    if (distance > this.swipeThreshold) {
+      this.back();
+    } else if (distance < -this.swipeThreshold) {
+      this.next();
+    }
   }
 }
