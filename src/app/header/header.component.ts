fbdedfe478b314985dc6965a5c9ab97748db8032
Eugen1188 <eugen.ferchow@gmx.de>
2026-02-27 00:12:35 +0100

    update mobile_nav

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  showMenu = false;
+
+  showMenuToggle() {
+    this.showMenu = !this.showMenu;
+  }
 
 }
