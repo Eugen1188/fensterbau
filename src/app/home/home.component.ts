f11bde7cc077d7365de7f971a7f46631749946ac
Eugen1188 <eugen.ferchow@gmx.de>
2026-03-05 00:47:57 +0100

    add benefits component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { AboutUsComponent } from "../about-us/about-us.component";
 import { ReferencesComponent } from "../references/references.component";
 import { ContactComponent } from "../contact/contact.component";
+import { BenefitsComponent } from "../benefits/benefits.component";
 
 @Component({
   selector: 'app-home',
-  imports: [AboutUsComponent, ReferencesComponent, ContactComponent],
+  imports: [AboutUsComponent, ReferencesComponent, ContactComponent, BenefitsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
