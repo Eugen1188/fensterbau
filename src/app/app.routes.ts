92f4051d452d5a61edb2a27c6dd97e4e358c3ce0
Eugen1188 <eugen.ferchow@gmx.de>
2026-03-06 17:23:13 +0100

    update footer,impress

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,19 @@
 import { Routes } from '@angular/router';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 import { HomeComponent } from './home/home.component';
+import { ImpressumComponent } from './impressum/impressum.component';
 
 export const routes: Routes = [
-      {
+  {
     path: '',
-    component: HomeComponent
+    component: HomeComponent,
   },
   {
     path: 'privacy-policy',
-    component: PrivacyPolicyComponent
-  }
-
+    component: PrivacyPolicyComponent,
+  },
+  {
+    path: 'impress',
+    component: ImpressumComponent,
+  },
 ];
--- src/app/impressum/impressum.component.ts
+++ src/app/impressum/impressum.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-impressum',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './impressum.component.html',
   styleUrl: './impressum.component.scss'
 })
