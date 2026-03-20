45603700015a3165923708e164618f7004ed53bb
Eugen1188 <eugen.ferchow@gmx.de>
2026-03-20 15:38:41 +0100

    test

--- sendmail.php
+++ sendmail.php
@@ -1,7 +1,18 @@
 <?php
 
 header("Content-Type: application/json; charset=utf-8");
-echo json_encode([
-    "success" => true,
-    "message" => "PHP läuft"
-]);
+
+try {
+    require __DIR__ . '/vendor/autoload.php';
+
+    echo json_encode([
+        "success" => true,
+        "message" => "autoload ok"
+    ]);
+} catch (Throwable $e) {
+    http_response_code(500);
+    echo json_encode([
+        "success" => false,
+        "error" => $e->getMessage()
+    ]);
+}
