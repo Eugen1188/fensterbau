f7e9581d15ff938a73891c8c5e4a20c5a2ab4da3
Eugen1188 <eugen.ferchow@gmx.de>
2026-03-21 01:15:28 +0100

    contact.comp fix

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -73,7 +73,7 @@ ${this.contactForm.value.message}
 `,
     };
 
-    this.http.post('/mail/sendMail.php', formData).subscribe({
+    this.http.post('/mail/sendmail.php', formData).subscribe({
       next: () => {
         this.loading = false;
         this.successMessage = true;
